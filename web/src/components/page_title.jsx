08863ebef267bc682bc0374729437d8aa05f581a
Sebastian Coates <sebass@Sebastians-MacBook-Pro-2.local>
2020-05-09 08:29:04 -0700

    Points in title, smaller sidebar

--- web/src/components/page_title.jsx
+++ web/src/components/page_title.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+    faIdBadge // I also like faReceipt
+} from '@fortawesome/free-solid-svg-icons'
 
 const PageTitle = ({pageName, score, crumbs}) => {
     let title = pageName ? `Immunify | ${pageName}` : "Immuto"
+    score = score || 0
     if (!pageName) {
         pageName = "Dashboard"
     }
@@ -9,12 +14,18 @@ const PageTitle = ({pageName, score, crumbs}) => {
     
     return (
         <span>
-            <h2 className="h2 text-gray mt-3">
+            <h2 className="h2 text-gray mt-3 page-title">
                {pageName}
-               {score ? <span className="float-right">
-                        <h5 className="user-score">{score}</h5>
-                        </span> 
-                      : ""}
+
+               <span className="float-right">
+                
+
+
+                <h2 className="user-score">
+                {score}
+                <FontAwesomeIcon className="ml-1" icon={faIdBadge} size="sm"/>
+                </h2>
+                </span> 
             </h2>
             <hr/>
             {crumbs}
